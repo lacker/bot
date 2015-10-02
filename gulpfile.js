495389118e5163985dc8480b08ddeb2255e5da74
Kevin Lacker <lacker@gmail.com>
2015-10-01 20:50:27 -0700

    set up gulp + babel

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+
+var path = require('path');
+var paths = {
+  src: ['src/*.js'],
+  build: 'build',
+  // Must be absolute or relative to source map
+  sourceRoot: path.join(__dirname, 'src'),
+};
+gulp.task('babel', function() {
+  return gulp.src(paths.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.', {sourceRoot: paths.sourceRoot}))
+    .pipe(gulp.dest(paths.build));
+});
+gulp.task('watch', function() {
+  gulp.watch(paths.src, ['babel']);
+});
+gulp.task('default', ['watch']);
