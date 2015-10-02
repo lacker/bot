bd516dedd0c09f5b2958d6fe461689a6da1a3156
Kevin Lacker <lacker@gmail.com>
2015-10-01 20:56:32 -0700

    stuff

--- build/main.js
+++ build/main.js
@@ -1,4 +1,14 @@
 'use strict';
 
-console.log('hello world');
+var Slack = require('slack-client');
+
+var token = 'xoxb-11756481184-QrFa2CKpvM7vYUbzW3ofoF7S';
+var autoReconnect = true;
+var autoMark = true;
+
+var slack = new Slack(token, autoReconnect, autoMark);
+
+slack.on('open', function () {
+  conso.log('connected.');
+});
 //# sourceMappingURL=main.js.map
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+let Slack = require('slack-client');
+
+let token = 'xoxb-11756481184-QrFa2CKpvM7vYUbzW3ofoF7S';
+let autoReconnect = true;
+let autoMark = true;
+
+let slack = new Slack(token, autoReconnect, autoMark);
+
+slack.on('open', () => {
+  conso.log('connected.');
+});
